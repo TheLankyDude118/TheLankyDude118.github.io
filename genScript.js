73b0991857cc1ce6c4e76bdecee70b6d824d22b9
TheLankyDude118 <77703779+TheLankyDude118@users.noreply.github.com>
2021-11-06 00:23:16 +0000

    Add files via upload

--- genScript.js
+++ genScript.js
@@ -1,14 +1,11 @@
 function btnHomeClick() {
-    window.close();
-    window.open("https://thelankydude118.github.io");
+    parent.location = "https://thelankydude118.github.io";
 }
 
 function btnProjClick() {
-    window.close();
-    window.open("https://thelankydude118.github.io/projects");
+    parent.location = "https://thelankydude118.github.io/projects";
 }
 
 function btnTestClick() {
-    window.close();
-    window.open("https://thelankydude118.github.io/scriptTest");
+    parent.location = "https://thelankydude118.github.io/scriptTest";
 }
